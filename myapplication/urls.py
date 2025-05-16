29f641ac07775676042d802f8964b44f612e0a1e
steve <gadafisteve001@gmail.com>
2025-05-16 17:36:22 +0300

    made updates

--- myapplication/urls.py
+++ myapplication/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.login_view , name="login"),
     path('logout/', views.logout_view, name='logout'),
+    path('admin_dashboard/' , views.admin_dashboard_view , name="admin_dashboard"),
 ]

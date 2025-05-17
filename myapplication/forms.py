3804c160b0a52de8d568bc8d71e7d59dbc539534
steve <gadafisteve001@gmail.com>
2025-05-17 15:39:55 +0300

    updated

--- myapplication/forms.py
+++ myapplication/forms.py
@@ -1,5 +1,6 @@
 
 from django import forms
+from .models import Patient
 
 class CustomLoginForm(forms.Form):
     username = forms.CharField(
@@ -14,3 +15,20 @@ class CustomLoginForm(forms.Form):
             'placeholder': 'Password'
         })
     )
+
+
+
+
+class PatientForm(forms.ModelForm):
+    class Meta:
+        model = Patient
+        fields = [
+            'first_name', 'last_name', 'date_of_birth', 'gender',
+            'id_number', 'phone_number', 'email', 'address',
+            'blood_type', 'allergies'
+        ]
+        widgets = {
+            'date_of_birth': forms.DateInput(attrs={'type': 'date'}),
+            'address': forms.Textarea(attrs={'rows': 2}),
+            'allergies': forms.Textarea(attrs={'rows': 3}),
+        }

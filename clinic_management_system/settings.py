ddc02af65a5357e8344ebe6fce1147ac5be466c7
steve <gadafisteve001@gmail.com>
2025-05-20 15:43:07 +0300

    made changes

--- clinic_management_system/settings.py
+++ clinic_management_system/settings.py
@@ -122,3 +122,12 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'innovationhubsoftwaresltd@gmail.com'
+EMAIL_HOST_PASSWORD = 'wtzg mvcn ztoe qige'
